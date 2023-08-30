5afc38ee920dd33abf2c48d89e5f16794c4361a6
Ankit <isankitraj@outlook.com>
2023-08-30 20:04:25 +0530

    added cart

--- 11_ecommerce-product-page/script.js
+++ 11_ecommerce-product-page/script.js
@@ -1,9 +1,15 @@
 "use strict";
 
+
+const body = document.querySelector('body')
 const sidebar = document.querySelector(".sidebar");
 const closeBtn = document.querySelector(".close");
 const menuBtn = document.querySelector(".menu");
 const overlay = document.querySelector(".overlay");
+const cart = document.querySelector('.cart')
+const cartOverview = document.querySelector('.cart-overview')
+
+
 
 menuBtn.addEventListener("click", function () {
   sidebar.classList.add("active");
@@ -15,4 +21,8 @@ closeBtn.addEventListener("click", function () {
 
 overlay.addEventListener('click', () => {
   sidebar.classList.remove("active");
+})
+
+cart.addEventListener('click', () => {
+  cartOverview.classList.toggle('show')
 })
